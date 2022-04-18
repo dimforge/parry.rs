1ef3eaa88c60d0810ac47e4b64d701cf2fa4dd37
Sébastien Crozet <developer@crozet.re>
2022-04-18 10:23:13 +0200

    Add base website

---
+++ sidebar_docs.js
@@ -0,0 +1,38 @@
+module.exports = {
+  docs: [
+    'about_parry',
+    {
+      'User Guide': [
+          'user_guide/getting_started',
+          // 'user_guide/geometric_representations',
+          // 'user_guide/bounding_volumes',
+          // 'user_guide/geometric_queries',
+          // 'user_guide/mesh_transformation',
+        ],
+    },
+    {
+      'API Documentation': [
+        {
+          type: 'link',
+          label: 'parry2d ↪',
+          href: 'https://docs.rs/parry2d'
+        },
+        {
+          type: 'link',
+          label: 'parry3d ↪',
+          href: 'https://docs.rs/parry3d'
+        },
+        {
+          type: 'link',
+          label: 'parry2d-f64 ↪',
+          href: 'https://docs.rs/parry2d-f64'
+        },
+        {
+          type: 'link',
+          label: 'parry3d-f64 ↪',
+          href: 'https://docs.rs/parry3d-f64'
+        },
+      ],
+    }
+  ],
+};
