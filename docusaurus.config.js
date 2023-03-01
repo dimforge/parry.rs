bcaad46747ca5ad406bc268d3c02a344125fc0cf
Sébastien Crozet <developer@crozet.re>
2023-03-01 12:03:43 +0100

    Add base website

---
+++ docusaurus.config.js
@@ -0,0 +1,157 @@
+const math = require('remark-math')
+const katex = require('rehype-katex')
+
+module.exports = {
+  title: 'Parry',
+  tagline: '2D and 3D collision-detection library for the Rust programming language.',
+  url: 'https://parry.rs',
+  baseUrl: '/',
+  onBrokenLinks: 'error', // 'throw',
+  favicon: 'img/favicon.png',
+  organizationName: 'dimforge', // Usually your GitHub org/user name.
+  projectName: 'parry', // Usually your repo name.
+  themeConfig: {
+    prism: {
+        theme: require('prism-react-renderer/themes/github'),
+        additionalLanguages: ['toml', 'rust'],
+    },
+    // announcementBar: {
+    //   id: 'supportus',
+    //   content:
+    //     '⭐️ If you like Parry, support us on <a target="_blank" rel="noopener noreferrer" href="https://github.com/sponsors/dimforge">GitHub Sponsor</a>! ⭐️',
+    // },
+    navbar: {
+      title: 'Parry',
+      logo: {
+        alt: 'Parry Logo',
+        src: 'img/logo_parry_wo_text.svg',
+      },
+      hideOnScroll: true,
+      items: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        },
+        {
+          to: '/community',
+          position: 'left',
+          activeBaseRegex: `/community/`,
+          label: 'Community',
+        },
+        {
+          href: 'https://dimforge.com/blog',
+          label: 'Blog ↪',
+          position: 'left',
+        },
+        // {to: 'blog', label: 'Blog', position: 'left'},
+        {
+          href: 'https://github.com/sponsors/dimforge',
+          label: 'Donate ♥',
+          position: 'right',
+          className: 'header-button-donate'
+        },
+        {
+          href: 'https://dimforge.com',
+          label: 'Dimforge ↪',
+          position: 'right',
+        },
+        {
+          href: 'https://github.com/dimforge/parry',
+          position: 'right',
+          className: 'header-github-link',
+          'aria-label': 'GitHub repository',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      logo: {
+        alt: 'Dimforge EURL Logo',
+        src: 'https://www.dimforge.com/img/logo/logo_dimforge_small_rect.svg',
+        href: 'https://www.dimforge.com/'
+      },
+      copyright: `Built by <a href="https://www.dimforge.com">Dimforge, EURL</a>.`,
+      links: [
+        {
+          title: 'Resources',
+          items: [
+            {
+              label: 'Documentation',
+              to: 'docs/',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/parry',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discord.gg/vt9DJSW',
+            },
+            // {
+            //   label: 'Twitter',
+            //   href: 'https://twitter.com/dimforge',
+            // },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            // {
+            //   label: 'Blog',
+            //   to: 'blog',
+            // },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/dimforge/parry',
+            },
+          ],
+        },
+      ],
+      // copyright: `Copyright © ${new Date().getFullYear()} Dimforge EURL. Website built with Docusaurus.`,
+    },
+  },
+  plugins: [
+    [
+      '@docusaurus/plugin-content-docs',
+      {
+        id: 'community',
+        path: 'community',
+        routeBasePath: 'community',
+        sidebarPath: require.resolve('./sidebar_community.js'),
+        showLastUpdateTime: false,
+      }
+    ],
+  ],
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebar_docs.js'),
+          showLastUpdateTime: false,
+          remarkPlugins: [math],
+          rehypePlugins: [katex],
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/dimforge/parry.rs/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+  stylesheets: [
+      'https://cdn.jsdelivr.net/npm/katex@0.11.0/dist/katex.min.css'
+  ]
+};
---
+++ src/pages/index.js
@@ -0,0 +1,182 @@
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import styles from './styles.module.css';
+
+const features = [
+  {
+    title: <>Complex shapes</>,
+    imageUrl: 'img/feature_complex_shapes.svg',
+    description: (
+      <>
+        Supports 2D and 3D shapes, from simple spheres to arbitrary
+        triangle meshes, heightfields, convex polyhedra.
+      </>
+    ),
+  },
+  {
+    title: <>Bounding volumes</>,
+    imageUrl: 'img/feature_bounding_volumes.svg',
+    description: (
+      <>
+        Bound complex shapes with simpler ones like AABB and bounding spheres
+        to perform efficient conservative geometric queries.
+      </>
+    ),
+  },
+  {
+    title: <>Ray casting</>,
+    imageUrl: 'img/feature_ray_casting.svg',
+    description: (
+      <>
+        Compute intersections between a ray and any shape. Typical
+        use-case include object selection and rendering (ray-tracing).
+      </>
+    ),
+  },
+  {
+    title: <>Point projection</>,
+    imageUrl: 'img/feature_point_projection.svg',
+    description: (
+      <>
+        Test a point for containment, compute distances to a point, or project it
+        on any shape.
+      </>
+    ),
+  },
+  {
+    title: <>Contact points</>,
+    imageUrl: 'img/feature_contact_points.svg',
+    description: (
+      <>
+        Find the closest points between objects in close proximity.  If they are
+        penetrating, the minimal translational distance can be obtained as well!
+      </>
+    ),
+  },
+  {
+    title: <>Sweep tests</>,
+    imageUrl: 'img/feature_time_of_impact.svg',
+    description: (
+      <>
+        Compute the time it would take for two moving shapes to start being in
+        contact.
+      </>
+    ),
+  },
+  {
+    title: <>Distance computation</>,
+    imageUrl: 'img/feature_smallest_distance.svg',
+    description: (
+      <>
+        Compute the global minimal distance between two shapes.
+      </>
+    ),
+  },
+  {
+    title: <>Convex hulls and decompositions</>,
+    imageUrl: 'img/hacd_small.png',
+    description: (
+      <>
+        Generate triangle meshes from smooth objects, compute their convex hull, or
+        decompose them approximately into their convex components.            </>
+    ),
+  },
+  {
+    title: <>Forever free and Open-Source</>,
+    imageUrl: 'img/undraw_join_of2w.svg',
+    description: (
+      <>
+        Built with a FOSS mindset, we aim to empower the Rust and web communities
+        with an efficient collision-detection framework.
+      </>
+    ),
+  },
+];
+
+function Feature({ imageUrl, title, description }) {
+  const imgUrl = useBaseUrl(imageUrl);
+  return (
+    <div className={clsx('col col--4', styles.feature)}>
+      {imgUrl && (
+        <div className="text--center">
+          <img className={styles.featureImage} src={imgUrl} alt={title} />
+        </div>
+      )}
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+function Home() {
+  const context = useDocusaurusContext();
+  const { siteConfig = {} } = context;
+  return (
+    <Layout
+      title={`${siteConfig.title} collision-detection library`}
+      description="Fast and cross-platform collision-detection library">
+      <header className={clsx('hero hero--primary', styles.heroBanner)}>
+        <div className="container">
+          <div className="">
+            <img src="img/logo_parry.svg" width="30%" alt="Project Logo" />
+          </div>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div className={styles.buttons}>
+            <Link
+              className={clsx(
+                'button button--outline button--lg --ifm-color-prim force-border', /*button--secondary*/
+                styles.getStarted,
+              )}
+              to={useBaseUrl('docs/user_guide/getting_started')}>
+              Get Started
+            </Link>
+          </div>
+        </div>
+      </header>
+      <main>
+        {features && features.length > 0 && (
+          <section className={styles.features}>
+            <div className="container">
+              <div className="row">
+                {features.map((props, idx) => (
+                  <Feature key={idx} {...props} />
+                ))}
+              </div>
+            </div>
+          </section>
+        )}
+        <section className={styles.sponsors}>
+          <div className="container" align={"center"}>
+            <h1>Sponsors</h1>
+            Parry is supported by the community and companies.
+            <hr />
+            <h2>Iridium Sponsors</h2>
+            <br />
+            <p>
+              <a href="https://croquet.io">
+                <img src="/img/croquet_logo.png" width="400px" />
+              </a>
+            </p>
+            <hr />
+            <h4>Gold Sponsors</h4>
+            <br />
+            <p>
+              <a href="https://fragcolor.com/">
+                <img src="/img/fragcolor_logo2_color_black.svg" width="300px" />
+              </a>
+              <a href="https://resolutiongames.com/">
+                <img src="/img/logo_resolution_games.png" width="300px" />
+              </a>
+            </p>
+          </div>
+        </section>
+      </main>
+    </Layout>
+  );
+}
+
+export default Home;
